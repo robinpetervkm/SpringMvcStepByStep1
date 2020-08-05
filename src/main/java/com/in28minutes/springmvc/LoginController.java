7b2e5b4dcc237c4ce50c2de08345c4d24ece5c1d
Robinclear <robin.peter.vkm@gmail.com>
2020-08-05 19:53:48 -0300

    View Resolver

--- src/main/java/com/in28minutes/springmvc/LoginController.java
+++ src/main/java/com/in28minutes/springmvc/LoginController.java
@@ -2,15 +2,13 @@
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 	
 	@RequestMapping(value = "/login")
-	@ResponseBody
 	public String sayHello() {
-		return "Hello World Dummy";
+		return "login";
 	}
 
 }
