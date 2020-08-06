a51d95ea7c93757081f85415055db1039dd9cc91
Robinclear <robin.peter.vkm@gmail.com>
2020-08-06 16:24:37 -0300

    What we will do: Learn about Spring Auto-wiring and Dependency Management. Use Auto-wiring to wire LoginService. @Autowired, @Service

--- src/main/java/com/in28minutes/login/LoginController.java
+++ src/main/java/com/in28minutes/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
-	private LoginService loginService = new LoginService();
+	//set LoginService As A Bean
+	@Autowired
+	private LoginService loginService ;
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String showLoginPage() {
--- src/main/java/com/in28minutes/login/LoginService.java
+++ src/main/java/com/in28minutes/login/LoginService.java
@@ -1,5 +1,9 @@
 package com.in28minutes.login;
 
+import org.springframework.stereotype.Service;
+
+//tell LoginService Is a New service to Spring
+@Service
 public class LoginService {
 	public boolean validateUser(String user, String password) {
 		return user.equalsIgnoreCase("in28Minutes") && password.equals("dummy");
