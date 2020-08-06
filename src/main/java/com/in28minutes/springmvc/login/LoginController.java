924dc250ea24a9821be03ceb47b43aff4177e273
Robinclear <robin.peter.vkm@gmail.com>
2020-08-06 08:00:38 -0300

    Added RequestParam in the Controller

--- src/main/java/com/in28minutes/springmvc/login/LoginController.java
+++ src/main/java/com/in28minutes/springmvc/login/LoginController.java
@@ -3,11 +3,17 @@ package com.in28minutes.springmvc.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String showLoginPage() {
 		return "login";
+	}
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String handleLoginResquest(@RequestParam("name") String name) {
+		System.out.println("NAME --- > "+name);
+		return "welcome";
 	}
 }
