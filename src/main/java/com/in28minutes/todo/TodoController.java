ca908e1f12b1a5a184d9e1bd14c671f1bfcd0152
Robinclear <robin.peter.vkm@gmail.com>
2020-08-07 00:44:14 -0300

    What we will do: Remove Hardcoding of User Name Remove LoginService Rename LoginController to WelcomeController Add Logout Functionality Useful Snippets 	private String getLoggedInUserName(ModelMap model) { 		Object principal = SecurityContextHolder.getContext() 				.getAuthentication().getPrincipal();
    		if (principal instanceof UserDetails)
    			return ((UserDetails) principal).getUsername();
    
    		return principal.toString();
    	}
    
    		<ul class="nav navbar-nav navbar-right">
    			<li><a href="/logout">Logout</a></li>
    		</ul>
    
    	@RequestMapping(value = "/logout", method = RequestMethod.GET)
    	public String logout(HttpServletRequest request,
    			HttpServletResponse response) {
    		Authentication auth = SecurityContextHolder.getContext()
    				.getAuthentication();
    		if (auth != null) {
    			new SecurityContextLogoutHandler().logout(request, response, auth);
    		}
    		return "redirect:/";
    	}

--- src/main/java/com/in28minutes/todo/TodoController.java
+++ src/main/java/com/in28minutes/todo/TodoController.java
@@ -7,6 +7,8 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -15,13 +17,11 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.in28minutes.model.Todo;
 import com.in28minutes.todo.service.TodoService;
 
 @Controller
-@SessionAttributes("name")
 public class TodoController {
 
 	@Autowired
@@ -36,7 +36,7 @@ public class TodoController {
 
 	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
 	public String showTodosList(ModelMap model) {
-		String user = getLoggedInUserName(model);
+		String user = getLoggedInUserName();
 		model.addAttribute("todos", service.retrieveTodos(user));
 		return "list-todos";
 	}
@@ -53,14 +53,20 @@ public class TodoController {
 		if (result.hasErrors())
 			return "todo";
 
-		service.addTodo(getLoggedInUserName(model), todo.getDesc(),
+		service.addTodo(getLoggedInUserName(), todo.getDesc(),
 				todo.getTargetDate(), false);
 		model.clear();// to prevent request parameter "name" to be passed
 		return "redirect:/list-todos";
 	}
 
-	private String getLoggedInUserName(ModelMap model) {
-		return (String) model.get("name");
+	private String getLoggedInUserName() {
+		Object principal = SecurityContextHolder.getContext()
+				.getAuthentication().getPrincipal();
+
+		if (principal instanceof UserDetails)
+			return ((UserDetails) principal).getUsername();
+
+		return principal.toString();
 	}
 
 	@RequestMapping(value = "/update-todo", method = RequestMethod.GET)
@@ -75,7 +81,7 @@ public class TodoController {
 		if (result.hasErrors())
 			return "todo";
 
-		todo.setUser(getLoggedInUserName(model));
+		todo.setUser(getLoggedInUserName());
 		service.updateTodo(todo);
 
 		model.clear();// to prevent request parameter "name" to be passed
